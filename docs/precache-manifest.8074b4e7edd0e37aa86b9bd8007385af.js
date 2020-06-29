cf7436e06de70efc0d5c5819e954eb8a079b8025
varun mandapati <varun_mandapati@comcast.com>
2020-06-29 09:02:11 -0400

    build

---
+++ docs/precache-manifest.8074b4e7edd0e37aa86b9bd8007385af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd28cbe71d358262394a44a541738277",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba99424f287cfd881498",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "89551127857b34b72418",
+    "url": "/static/css/main.cc6ab546.chunk.css"
+  },
+  {
+    "revision": "ba99424f287cfd881498",
+    "url": "/static/js/2.be46ea04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.be46ea04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89551127857b34b72418",
+    "url": "/static/js/main.74298525.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
